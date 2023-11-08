35d4ce1f8d947cb5a83057c1198d4a0e3aed86ec
Mariya-MX <93935212+Mariya-MX@users.noreply.github.com>
2023-11-08 08:17:50 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -11,8 +11,19 @@ urlpatterns = [
    path('customer_profile/',views.customer_profile,name="customer_profile"),
    path('logout/',views.userLogout,name="logout"),
    path('services/',views.services,name="services"),
+   path('profile/',views.profile,name="profile"),
+   path('custom_admin_panel/', views.custom_admin_panel, name="custom_admin_panel"),
+   path('userlist/',views.userlist,name="userlist"),
+   path('home/',views.home,name="home"),
+   path('technician/',views.technician,name="technician"),
+   path('technician_info/', views.technician_info, name='technician_info'),
+   path('application/', views.application, name='application'),
+   path('change_password/', views.change_password, name='change_password'),
+   path('approval/', views.approval, name='approval'),
+   path('deactivate_user/<int:user_id>/', views.deactivate_user, name='deactivate_user'),
 
   
+
 
 
     path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
