d8f34b50ed42044b6847b16521ae89511cab7531
Mariya-MX <93935212+Mariya-MX@users.noreply.github.com>
2023-11-08 08:15:51 +0530

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from .models import CustomUser  # Import your model
+from django.contrib.auth import get_user_model
+from .models import UserProfile
 
-admin.site.register(CustomUser)  # Register your model with the admin interface
+
+User = get_user_model()
+
+class SuperuserAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return User.objects.filter(is_superuser=False)
+    list_display=('username','email','user_type')
+
+# Register the custom admin class
+admin.site.register(User, SuperuserAdmin)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'image', 'first_name', 'last_name', 'mobile', 'address')  # Customize this according to your UserProfile model
+
+# Register the custom admin class for UserProfile
+admin.site.register(UserProfile, UserProfileAdmin)
